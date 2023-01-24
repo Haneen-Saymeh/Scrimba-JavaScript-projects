391e448ac9306bea671a78cfb12f114a94e5ebe0
Haninz <enghaneen_s@hotmail.com>
2023-01-24 19:26:41 +0200

    black jack dooone

--- JavaScript asseignments/Blackjack game/app.js
+++ JavaScript asseignments/Blackjack game/app.js
@@ -1,14 +1,50 @@
-let firstCard=10
-let secondCard =5
-let sum = firstCard+secondCard+7
+
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = " "
 
 let messageEl = document.getElementById("msg")
-let sumEle= document.querySelector("#sum-ele")
+let sumEle= document.getElementById("sum-ele")
+let cardEle= document.getElementById("card-ele")
+
+let player={
+    name:"Hanin" ,
+    chips: "500"
+
+}
+let playerEle= document.getElementById("player-ele")
+playerEle.textContent = player.name + ":  $" + player.chips
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 
 function startgame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    rendergame()
+}
+
+
+function rendergame(){
+    cardEle.textContent= "cards: " 
+    for (i=0; i<cards.length; i++){
+        cardEle.textContent += cards[i] + " "
+    }
 sumEle.textContent ="Sum : " + sum
 
 if (sum <21){
@@ -28,4 +64,15 @@ messageEl.textContent = message
 
 }
 
-startgame()
+
+function newcard(){
+    if (isAlive === true && hasBlackJack === false){
+
+    let card  = getRandomCard() 
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    rendergame()
+
+    }
+}
